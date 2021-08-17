90da04850e5520844edec0b72af8dab04527ce59
Panda0412 <nasb_99@mail.ru>
2021-08-17 19:50:36 +0300

    minor change

--- src/ColoredUserpic.js
+++ src/ColoredUserpic.js
@@ -13,18 +13,18 @@ function ColoredUserpic(props) {
                 onMouseEnter={() => setHover(true)}
                 onMouseLeave={() => setHover(false)}
                 style={{
-                    maxHeight: props.size + 'px',
-                    maxWidth: props.size + 'px',
+                    height: props.size + 'px',
+                    width: props.size + 'px',
                     borderRadius: '50%',
-                    background: hover ? `linear-gradient(to right, ${props.hoverColors})` : `linear-gradient(to right, ${props.colors})`,
-                    padding: props.colorWidth + 'px'
+                    background: hover ? `linear-gradient(to right, ${props.hoverColors})` : `linear-gradient(to right, ${props.colors})`
                 }}
             >
                 <div style={{
                     width: avatarBackgroundSize + 'px',
                     height: avatarBackgroundSize + 'px',
                     borderRadius: '50%',
-                    background: props.backgroundColor
+                    background: props.backgroundColor,
+                    margin: props.colorWidth + 'px'
                 }}>
                     <img src={avatar} alt={'Avatar'} style={{
                         width: avatarSize + 'px',
